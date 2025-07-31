4b0ef54bcf4231b6e84fb28b6c2324a24431cc48
waqar95 <waqar95ahmed@gmail.com>
2025-07-31 18:58:19 +0200

    Styled To-Do list with SCSS

--- src/app/component/todo/todo.component.ts
+++ src/app/component/todo/todo.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Todo {
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
+  newTask = '';
+  todos: Todo[] = [];
 
+  addTodo() {
+    if (this.newTask.trim()) {
+      this.todos.push({ task: this.newTask.trim(), completed: false });
+      this.newTask = '';
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
