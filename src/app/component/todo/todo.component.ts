63d5f2b45844b06d02051af2fd3454445168db47
waqar95 <waqar95ahmed@gmail.com>
2025-08-03 23:48:21 +0200

    Added local storage support for todos

--- src/app/component/todo/todo.component.ts
+++ src/app/component/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -12,20 +12,33 @@ interface Todo {
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css'],
+  styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   newTask = '';
   todos: Todo[] = [];
+
+  ngOnInit(): void {
+    const saved = localStorage.getItem('todos');
+    if (saved) {
+      this.todos = JSON.parse(saved);
+    }
+  }
 
   addTodo() {
     if (this.newTask.trim()) {
       this.todos.push({ task: this.newTask.trim(), completed: false });
       this.newTask = '';
+      this.saveTodos();
     }
   }
 
   removeTodo(index: number) {
     this.todos.splice(index, 1);
+    this.saveTodos();
+  }
+
+  saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
