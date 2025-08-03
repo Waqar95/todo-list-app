27b15c26b4ada339b0505c2c6180e6ba1d457e03
waqar95 <waqar95ahmed@gmail.com>
2025-08-04 00:02:16 +0200

    Integrated Angular Material and icons into To-Do List

--- src/app/component/todo/todo.component.ts
+++ src/app/component/todo/todo.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 interface Todo {
   task: string;
@@ -10,9 +15,17 @@ interface Todo {
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule, FormsModule],
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
 })
 export class TodoComponent implements OnInit {
   newTask = '';
